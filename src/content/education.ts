02d7047313417cbcb8e285dff3646041daea8adc
leonardpleiss <leonard.pleiss@tum.de>
2026-03-08 21:06:54 +0100

    Update education.ts

--- src/content/education.ts
+++ src/content/education.ts
@@ -18,13 +18,13 @@ export const education: Education[] = [
     institution: "Technical University Berlin",
     period: "2014 — 2016",
     description:
-      "Focus on statistical learning. Thesis: Localizing emotional processes within the human cerebral cortex using fMRI data. Supervised by Prof. Obermayer and Prof. Deniz. GPA: 4.0/4.0.",
+      "",
   },
   {
     degree: "B.Sc. in Psychology",
     institution: "University of Potsdam",
     period: "2016 — 2019",
     description:
-      "Focus on cognitive science. Thesis: Investigating the relationship between media multitasking and cognitive-executive performance. Supervised by Dr. Soemer and Prof. Kliegl.",
+      "",
   },
 ];
